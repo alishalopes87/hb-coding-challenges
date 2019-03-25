92ea89b37e7d801e66976b8a19c9b42872a458dd
Alisha Lopes <alishalopes@MacBook-Pro-3.localdomain>
2019-03-25 11:24:14 -0700

    Reverse string recursively

---
+++ reverse_string_recursively.py
@@ -0,0 +1,43 @@
+"""Reverse a string using recursion.
+
+For example::
+
+    >>> rev_string("")
+    ''
+
+    >>> rev_string("a")
+    'a'
+
+    >>> rev_string("porcupine")
+    'enipucrop'
+
+"""
+
+
+def rev_string(astring):
+    """Return reverse of string using recursion.
+
+    You may NOT use the reversed() function!
+    """
+    #base caes is given string is empty
+    #create new string variable 
+    #work towards base case by slicing astring each time one index at the end
+    #concat astring slice each time to new_string
+    
+
+
+    if not astring:
+        return astring
+
+    return  astring[-1:] + rev_string(astring[:-1])
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. !KROW DOOG\n")
